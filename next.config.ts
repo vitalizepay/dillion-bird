70cf2a9bc1c8ed5c064937ae6374e27c7cce8242
vitalizepay <vitalizepay@gmail.com>
2026-01-14 17:00:27 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',      // Enables the 'out' folder required by your GitHub Action
+  images: {
+    unoptimized: true,   // Required because GitHub Pages is a static host
+  },
+  /* You can add other config options here */
 };
 
 export default nextConfig;

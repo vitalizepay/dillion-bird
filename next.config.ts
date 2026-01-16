3471f6e7dcce28ee7668cc2a21f87e7bb633bfeb
vitalizepay <vitalizepay@gmail.com>
2026-01-16 13:19:55 +0530

    changed image path in next confif

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  // Add these two lines below:
-  basePath: '/dillion-bird',
-  assetPrefix: '/dillion-bird',
+
 };
 
 export default nextConfig;
